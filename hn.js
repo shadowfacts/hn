190386a3f486ff800d81db44a580b6b450510cf4
Shadowfacts <me@shadowfacts.net>
2016-10-05 13:59:52 -0400

    More random work

--- hn.js
+++ hn.js
@@ -1,67 +1,66 @@
-function newStories(start, end) {
-	return req(endpoint("newstories.json"))
-		.then((res) => {
-			if (amount) {
-				return res.data.splice(start, end);
-			}
-			return res.data;
-		});
+function createHN() {
+	var req = function(url) {
+		return axios.get(url)
+			.catch(console.error);
+	}
+
+	var endpoint = function(path) {
+		return `https://hacker-news.firebaseio.com/v0/${path}`;
+	}
+
+	return {
+		new: function(start, end) {
+			return req(endpoint("newstories.json"))
+				.then((res) => {
+					if (amount) {
+						return res.data.slice(start, end);
+					}
+					return res.data;
+				});
+		},
+		top: function(start, end) {
+			return req(endpoint("topstories.json"))
+				.then((res) => {
+					return res.data.slice(start, end);
+				});
+		},
+		best: function(start, end) {
+			return req(endpoint("beststories.json"))
+				.then((res) => {
+					return res.data.slice(start, end);
+				});
+		},
+		ask: function(start, end) {
+			return req(endpoint("askstories.json"))
+				.then((res) => {
+					return res.data.slice(start, end);
+				});
+		},
+		show: function(start, end) {
+			return req(endpoint("showstories.json"))
+				.then((res) => {
+					return res.data.slice(start, end);
+				});
+		},
+		jobs: function(start, end) {
+			return req(endpoint("jobstories.json"))
+				.then((res) => {
+					return res.data.slice(start, end);
+				});
+		},
+		item: function(id) {
+			return req(endpoint(`item/${id}.json`))
+				.then((res) => {
+					return res.data;
+				});
+		},
+		user: function(name) {
+			return req(endpoint(`user/${name}.json`))
+				.then((res) => {
+					return res.data;
+				});
+		}
+	};
 }
 
-function topStories(start, end) {
-	return req(endpoint("topstories.json"))
-		.then((res) => {
-			return res.data.splice(start, end);
-		});
-}
-
-function bestStories(start, end) {
-	return req(endpoint("beststories.json"))
-		.then((res) => {
-			return res.data.splice(start, end);
-		});
-}
-
-function askStories(start, end) {
-	return req(endpoint("askstories.json"))
-		.then((res) => {
-			return res.data.splice(start, end);
-		});
-}
-
-function showStories(start, end) {
-	return req(endpoint("showstories.json"))
-		.then((res) => {
-			return res.data.splice(start, end);
-		});
-}
-
-function jobStories(start, end) {
-	return req(endpoint("jobstories.json"))
-		.then((res) => {
-			return res.data.splice(start, end);
-		});
-}
-
-function item(id) {
-	return req(endpoint(`item/${id}.json`))
-		.then((res) => {
-			return res.data;
-		});
-}
-
-function user(name) {
-	return req(endpoint(`user/${name}.json`))
-		.then((res) => {
-			return res.data;
-		});
-}
-
-function req(url) {
-	return axios.get(url)
-		.catch(console.error);
-}
-
-function endpoint(path) {
-	return `https://hacker-news.firebaseio.com/v0/${path}`;
-}
+let hn = createHN();
--- main.js
+++ main.js
@@ -1,12 +1,45 @@
-let list = $("#stories");
+var loaded = 0;
+let hash = window.location.hash.substring(1);
 
-load(0, 15);
+if (hash.length != 0 && !isNaN(hash)) { // viewing an item
+	displayItem(hash);
+} else { // display posts list
+	loadPosts();
+}
 
-function load(start, end) {
-	topStories(start, end)
+function displayItem(id) {
+	hn.item(id)
+		.then((item) => {
+			// TODO: me
+		});
+}
+
+function loadPosts() {
+	let list = $("<ul></ul>");
+	list.attr("id", "stories");
+	list.appendTo(document.body);
+
+	let btn = $("<button></button>");
+	btn.attr("id", "more");
+	btn.text("More...");
+	btn.appendTo(document.body);
+	btn.click(() => {
+		load(loaded);
+	});
+
+	load(loaded);
+	// TODO: go to next page
+}
+
+function load(start) {
+	loaded += 15;
+
+	let list = $("#stories");
+
+	hn.top(start, start + 15)
 		.then((stories) => {
 			stories.forEach((id) => {
-				item(id)
+				hn.item(id)
 					.then((item) => {
 						list.append(createItem(item));
 					});
@@ -15,19 +48,26 @@ function load(start, end) {
 }
 
 function createItem(item) {
+	var kids = function() {
+		if (item.kids) {
+			return `<span class="comments">${item.kids.length}</span>`;
+		} else {
+			return "";
+		}
+	}
+
 	return `
 <li>
 	<div class="story">
 		<section class="left">
-			<span class="title">${item.title}</span>
-			<br>
-			<span class="domain">${getDomain(item.url)}</span>
+			<p class="title">${item.title}</p>
+			<p class="domain">${getDomain(item.url)}</p>
 		</section>
 		<section class="right">
-			<span class="time">16h</span>
+			<span class="time">${ago(item.time)}</span>
 			<br>
 			<span class="votes">${item.score}</span>
-			<span class="comments">${item.kids.length}</span>
+			<span class="comments">${item.kids ? item.kids.length : 0}</span>
 		</section>
 	</div>
 </li>
@@ -35,12 +75,42 @@ function createItem(item) {
 }
 
 function getDomain(url) {
-	let afterProtocol = url.split("://")[1];
-	let parts = afterProtocol.split("/");
-	let domain = parts[0];
-	if (domain.startsWith("www.")) {
-		return domain.substring(4);
+	if (url) {
+		let afterProtocol = url.split("://")[1];
+		let parts = afterProtocol.split("/");
+		let domain = parts[0];
+		if (domain.startsWith("www.")) {
+			return domain.substring(4);
+		} else {
+			return domain;
+		}
+	}
+	return "";
+}
+
+let MILIS = 1;
+let SECOND = MILIS * 1000;
+let MINUTE = SECOND * 60;
+let HOUR = MINUTE * 60;
+let DAY = HOUR * 24;
+let MONTH = DAY * 30;
+let YEAR = DAY * 365;
+
+function ago(time) {
+	let delta = Date.now() - (time * 1000);
+	if (delta >= YEAR) {
+		return Math.floor(delta / YEAR) + " yrs";
+	} else if (delta >= MONTH) {
+		return Math.floor(delta / MONTH) + " months";
+	} else if (delta >= DAY) {
+		return Math.floor(delta / DAY) + " days";
+	} else if (delta >= HOUR) {
+		return Math.floor(delta / HOUR) + " hrs";
+	} else if (delta >= MINUTE) {
+		return Math.floor(delta / MINUTE) + " min"
+	} else if (delta >= SECOND) {
+		return Math.floor(delta / SECOND) + " secs";
 	} else {
-		return domain;
+		return delta + " ms";
 	}
 }
