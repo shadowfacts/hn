033efb8dab6748775d96cea2554993a76b2b607f
Shadowfacts <me@shadowfacts.net>
2016-12-09 13:02:23 -0500

    Don't pluralize time unit, fixes overflowing on index page

--- js/util.js
+++ js/util.js
@@ -10,22 +10,22 @@ function ago(time) {
 	const delta = Date.now() - (time * 1000);
 	if (delta >= YEAR) {
 		const years = Math.floor(delta / YEAR);
-		return years + " year" + getPluralEnding(years);
+		return years + " yr";
 	} else if (delta >= MONTH) {
 		const months = Math.floor(delta / MONTH);
-		return months + " month" + getPluralEnding(months);
+		return months + " month";
 	} else if (delta >= DAY) {
 		const days = Math.floor(delta / DAY);
-		return days + " day" + getPluralEnding(days);
+		return days + " day";
 	} else if (delta >= HOUR) {
 		const hours = Math.floor(delta / HOUR);
-		return hours + " hr" + getPluralEnding(hours);
+		return hours + " hr";
 	} else if (delta >= MINUTE) {
 		const minutes = Math.floor(delta / MINUTE);
-		return minutes + " min" + getPluralEnding(minutes);
+		return minutes + " min";
 	} else if (delta >= SECOND) {
 		const seconds = Math.floor(delta / SECOND);
-		return seconds + " sec" + getPluralEnding(seconds);
+		return seconds + " sec";
 	} else {
 		return delta + " ms";
 	}
